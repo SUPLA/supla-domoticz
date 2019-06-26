35f6de0bc53a945cc2e37f30f747ab8617e3daae
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2019-06-26 21:06:55 +0200

    Rename folder 'api' to 'supla_api'

--- plugin.py
+++ plugin.py
@@ -1,6 +1,6 @@
-from api import *
+from supla_api import *
 
 token = "MzFhYTNiZTAwODg5M2E0NDE3OGUwNWE5ZjYzZWQ2YzllZGFiYWRmNDQwNDBlNmZhZGEzN2I3NTJiOWM2ZWEyZg.aHR0cDovL2xvY2FsaG9zdDo5MDkw"
-x = api.ApiClient(token)
+x = supla_api.ApiClient(token)
 
 print(x.url)
---
+++ supla_api/__init__.py
@@ -0,0 +1,1 @@
+__all__ = ["supla_api"]
