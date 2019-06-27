be690557fdb4bca55a0ac60e7fe50289e2e2fe8f
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2019-06-27 20:09:00 +0200

    Use pytest

--- supla_api/supla_api_test.py
+++ supla_api/supla_api_test.py
@@ -1,14 +1,15 @@
-import unittest
-from api import *
+from supla_api import *
 
 
-class ApiTest(unittest.TestCase):
-    def test_token(self):
-        token = "MzFhYTNiZTAwODg5M2E0NDE3OGUwNWE5ZjYzZWQ2YzllZGFiYWRmNDQwNDBlNmZhZGEzN2I3NTJiOWM2ZWEyZg.aHR0cDovL2xvY2FsaG9zdDo5MDkw"
-        api_client = ApiClient(token)
-        self.assertEqual(api_client.token, token)
+def test_token():
+    token = "MzFhYTNiZTAwODg5M2E0NDE3OGUwNWE5ZjYzZWQ2YzllZGFiYWRmNDQwNDBlNmZhZGEzN2I3NTJiOWM2ZWEyZg" \
+            ".aHR0cDovL2xvY2FsaG9zdDo5MDkw"
+    api_client = supla_api.ApiClient(token)
+    assert api_client.token == token
 
-    def test_url(self):
-        token = "MzFhYTNiZTAwODg5M2E0NDE3OGUwNWE5ZjYzZWQ2YzllZGFiYWRmNDQwNDBlNmZhZGEzN2I3NTJiOWM2ZWEyZg.aHR0cDovL2xvY2FsaG9zdDo5MDkw"
-        api_client = ApiClient(token)
-        self.assertEqual(api_client.url, "http://localhost:9090")
+
+def test_url():
+    token = "MzFhYTNiZTAwODg5M2E0NDE3OGUwNWE5ZjYzZWQ2YzllZGFiYWRmNDQwNDBlNmZhZGEzN2I3NTJiOWM2ZWEyZg" \
+            ".aHR0cDovL2xvY2FsaG9zdDo5MDkw"
+    api_client = supla_api.ApiClient(token)
+    assert api_client.url == "http://localhost:9090"
