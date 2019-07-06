b97435cf6a081a6071c3289eefddd6525514f941
Martin Grzeslowski <martin.grzeslowski@gmail.com>
2019-07-06 19:40:18 +0200

    #1 Fix tests

--- supla_api/supla_api_test.py
+++ supla_api/supla_api_test.py
@@ -4,12 +4,12 @@ from supla_api import *
 def test_token():
     token = "MzFhYTNiZTAwODg5M2E0NDE3OGUwNWE5ZjYzZWQ2YzllZGFiYWRmNDQwNDBlNmZhZGEzN2I3NTJiOWM2ZWEyZg" \
             ".aHR0cDovL2xvY2FsaG9zdDo5MDkw"
-    api_client = supla_api.ApiClient(token)
+    api_client = supla_api.ApiClient(token, lambda msg: print(msg), lambda msg: print(msg))
     assert api_client.token == token
 
 
 def test_url():
     token = "MzFhYTNiZTAwODg5M2E0NDE3OGUwNWE5ZjYzZWQ2YzllZGFiYWRmNDQwNDBlNmZhZGEzN2I3NTJiOWM2ZWEyZg" \
             ".aHR0cDovL2xvY2FsaG9zdDo5MDkw"
-    api_client = supla_api.ApiClient(token)
+    api_client = supla_api.ApiClient(token, lambda msg: print(msg), lambda msg: print(msg))
     assert api_client.url == "http://localhost:9090"
